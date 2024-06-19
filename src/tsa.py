5ff36c941bf8b5bab8c6bba5c9ac59a549d5a256
Barney <barney.potter.24@gmail.com>
2024-06-19 17:19:32 +0200

    initial commit

---
+++ src/tsa.py
@@ -0,0 +1,13 @@
+"""tsa.py
+
+This module contains the Temporal Signal Analysis core functionality.
+"""
+from argh import dispatch_command
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    
+    dispatch_command(main)
+    
